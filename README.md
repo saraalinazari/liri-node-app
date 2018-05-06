70790b21bed5c7657c95a0c5bed2b847a02921f2
saraalinazari <sara.alinazari@gmail.com>
2018-05-06 11:13:10 -0700

    First

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # liri-node-app
+1. If you type "my-tweets", it shows your tweets at the most 20.
+2. If you type "post-my-tweets" + your favorite text, then you can post your tweet.
+3. If you type "spotify-this-song" + the name of song, then you can see the song specifications.
+4. If you type "movie-this" + the name of the movie, then you can see the movie name.
+5. If you type "do-what-it-says", then you can run the random.txt command.
+6. All your commands and outputs are logged into log.txt
--- liri.js
+++ liri.js
@@ -4,24 +4,33 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-//var omdb = require('omdb');
 var request = require('request');
 var fs = require('fs');
-console.log("spotify"+JSON.stringify( spotify));
-console.log("twitter" +JSON.stringify(client ));
+//console.log("spotify"+JSON.stringify( spotify));
+//console.log("twitter" +JSON.stringify(client ));
 var currenttime=new Date();
+//var today = new Date();
+var dd = currenttime.getDate();
+var mm = currenttime.getMonth()+1; //January is 0!
+var yyyy = currenttime.getFullYear();
+currenttime = mm + '/' + dd + '/' + yyyy;
+
+
 var whattodo = process.argv[2];
 
 var searchItem = "";
 for(var i=3;i<process.argv.length;i++){
     searchItem += process.argv[i]+" ";
 }
-//process.argv[3]; 
+
 run();
 function run(){
     switch(whattodo){
         case "my-tweets":
             twitt();
+            break;
+        case "post-my-tweets":
+            posttwitt();
             break;
         case "spotify-this-song":
             spotifySong();
@@ -35,6 +44,7 @@ function run(){
         
     }
 }
+var str="";
 function twitt(){
     var params = {
         count : 20
@@ -42,40 +52,61 @@ function twitt(){
     var tweetObjs;
     client.get('statuses/user_timeline', params,function(error, tweets, response){
         if(!error){
-            console.log(tweets);
+           
             tweetObjs = tweets;
-           // console.log(response);
+          
         }
-        var str ="Command:"+whattodo+" "+ searchItem+" "+"\n Time"+currenttime.getDate();
+        str ="Command:"+whattodo+" "+ searchItem+" , "+" Time: "+currenttime;
         
         for(var eachitem in tweets){
-            console.log("***"+i+"****"+tweets[eachitem].text);
-            str+= "***"+i+"****"+tweets[eachitem].text;
+            var temp = parseInt(eachitem)  + 1;
+            console.log(temp+"-"+"tweets text: "+tweets[eachitem].text);
+            console.log("  tweet time: "+tweets[eachitem].created_at);
+            str+= temp+"-"+"tweets text: "+tweets[eachitem].text+"  tweet time: "+tweets[eachitem].created_at;
+
         }
+        str+= "\n"+ "===============================================================\n";
         log(str);
+        str="";
     });   
+}
+function posttwitt(){
+    //str ="Command:"+whattodo+" "+ searchItem+" "+"\n Time"+currenttime.getDate();
+    var update = {
+        status: searchItem
+    }
+    client.post('statuses/update', update,  function(error, tweet, response) {
+        if(error) {console.log(error);}
+      
+      });
+      str ="Command:"+whattodo+" "+ searchItem+" , "+" Time: "+currenttime;
+      str+= "\n"+ "===============================================================\n";
+      log(str);
+      str="";
 }
 function spotifySong(){
     var params = {
          type: 'track', 
          query: searchItem 
     }
-   // console.log(searchItem);
+   
     spotify.search(params,function(error,data){
-        var str ="Command:"+whattodo+" "+ searchItem+" "+"\n Time"+currenttime.getDate();
+        var str ="Command:"+whattodo+" "+ searchItem+" , "+" Time: "+currenttime;
         if(!error){
             for(var eachitem in data.tracks.items){
-                console.log("*****"+eachitem+"1 spotify****"+(data.tracks.items[eachitem].artists[0].name) );
-                str +="*****"+eachitem+"1 spotify****"+(data.tracks.items[eachitem].artists[0].name);
-                console.log("*****"+eachitem+"2 spotify****"+JSON.stringify(data.tracks.items[eachitem].name) );
-                str += "*****"+eachitem+"2 spotify****"+JSON.stringify(data.tracks.items[eachitem].name);
-                console.log("*****"+eachitem+"3 spotify****"+JSON.stringify(data.tracks.items[eachitem].preview_url));
-                str +="*****"+eachitem+"3 spotify****"+JSON.stringify(data.tracks.items[eachitem].preview_url);
-                console.log("*****"+eachitem+"4 spotify****"+JSON.stringify(data.tracks.items[eachitem].album.name));
-                str +="*****"+eachitem+"4 spotify****"+JSON.stringify(data.tracks.items[eachitem].album.name);
+                console.log(eachitem+"-Artist Name: "+(data.tracks.items[eachitem].artists[0].name) );
+                str +=eachitem+"-Artist Name: "+(data.tracks.items[eachitem].artists[0].name);
+                console.log("\n  Song: "+JSON.stringify(data.tracks.items[eachitem].name) );
+                str += "\n  Song: "+JSON.stringify(data.tracks.items[eachitem].name);
+                console.log("\n  URL: "+JSON.stringify(data.tracks.items[eachitem].preview_url));
+                str +="\n  URL: "+JSON.stringify(data.tracks.items[eachitem].preview_url);
+                console.log("\n  Album Name: "+JSON.stringify(data.tracks.items[eachitem].album.name+"\n"));
+                str +="\n  Album Name: "+JSON.stringify(data.tracks.items[eachitem].album.name+"\n");
             }
         }
+        str+= "\n"+ "===============================================================\n";
         log(str);
+        str="";
     });
 }
 function OmdbCall(){
@@ -86,27 +117,30 @@ function OmdbCall(){
         title: searchItem
     }//JSON.parse(body)
     request('http://www.omdbapi.com/?i=tt3896198&apikey=adfa6881&plot=full&t='+params.title, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('statusCode:',(JSON.parse(body).Response));
-    var str ="Command:"+whattodo+" "+ searchItem+" "+"\n Time"+currenttime.getDate();
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+   // console.log('statusCode:',(JSON.parse(body).Response));
+    str ="\nCommand: "+whattodo+" "+ searchItem+" "+"\n Time: "+currenttime;
     if(JSON.parse(body).Response==="False"){
         console.log("not found");
         str += "not found";
     }
     else{
-        console.log('Title: ',JSON.parse(body).Title +"\n"+"Year: "+JSON.parse(body).Year+"\n"+"Ratings: "+JSON.parse(body).Ratings[0].Source+","+JSON.parse(body).Ratings[0].Value+"\n"+"Ratings: "+JSON.parse(body).Ratings[1].Source+","+JSON.parse(body).Ratings[1].Value+"\n"+"Country: "+
-    JSON.parse(body).Country+"\n"+"Language: "+JSON.parse(body).Language+"\n"+"Plot: "+JSON.parse(body).Plot+"\n"+"Actors: "+JSON.parse(body).Actors); // Print the HTML for the Google homepage.
-        str += 'Title: ',JSON.parse(body).Title +"\n"+"Year: "+JSON.parse(body).Year+"\n"+"Ratings: "+JSON.parse(body).Ratings[0].Source+","+JSON.parse(body).Ratings[0].Value+"\n"+"Ratings: "+JSON.parse(body).Ratings[1].Source+","+JSON.parse(body).Ratings[1].Value+"\n"+"Country: "+
-        JSON.parse(body).Country+"\n"+"Language: "+JSON.parse(body).Language+"\n"+"Plot: "+JSON.parse(body).Plot+"\n"+"Actors: "+JSON.parse(body).Actors;
+        console.log(' Title: ',JSON.parse(body).Title +"\n "+"Year: "+JSON.parse(body).Year+"\n "+" Ratings: "+JSON.parse(body).Ratings[0].Source+"/"+JSON.parse(body).Ratings[0].Value+"\n"+" Ratings: "+JSON.parse(body).Ratings[1].Source+"/"+JSON.parse(body).Ratings[1].Value+"\n "+"Country: "+
+    JSON.parse(body).Country+"\n"+"Language: "+JSON.parse(body).Language+"\n"+"Plot: "+JSON.parse(body).Plot+"\n"+"Actors: "+JSON.parse(body).Actors+"\n"); // Print the HTML for the Google homepage.
+        str += '\n Title: '+ JSON.parse(body).Title +"\n "+"Year: "+JSON.parse(body).Year+"\n "+"Ratings: "+JSON.parse(body).Ratings[0].Source+"  "+JSON.parse(body).Ratings[0].Value+"\n"+" Ratings: "+JSON.parse(body).Ratings[1].Source+"  "+JSON.parse(body).Ratings[1].Value+"\n "+"Country: "+
+        JSON.parse(body).Country+"\n"+" Language: "+JSON.parse(body).Language+"\n"+" Plot: "+JSON.parse(body).Plot+"\n"+" Actors: "+JSON.parse(body).Actors+"\n"; // Print the HTML for the Google homepage.
+        str+= "\n"+ "===============================================================\n";
+        log(str);
+        str="";
     }
-    log(str);
+   
 });
     
 }
 function fileRead(){
     var whatArr;
-    var str ="Reading from Random.txt:"+" "+"\n Time"+currenttime.getDate();
+    str ="Reading from Random.txt:"+" , "+" Time: "+currenttime;
     log(str);
    fs.readFile("random.txt","utf8",function(error,data){
         if(!error){
@@ -119,6 +153,7 @@ function fileRead(){
             run();
         }
     });
+    str="";
 }
 function log(strToAdd){
     fs.appendFile("log.txt",strToAdd,function(error){
